ead5da86ef212c90146ea6f7fb9142a2cd40b74f
Matthew Ruiz <mrr171@txstate.edu>
2024-10-02 13:29:55 -0500

    need to update

---
+++ A_star/ard_astar/Bitmap.h
@@ -0,0 +1,45 @@
+#ifndef BITMAP_HPP
+#define BITMAP_HPP
+
+#include <Cpp_Standard_Library.h>
+//#include <std::vector.h>
+// replace fstream with sd card read/write library
+//#include <fstream>
+#include <stdio.h>
+// replace io code with serial print or delete
+//#include <iostream>
+// replace remove_if functionality
+//#include <algorithm>
+
+const int MAX_WIDTH = 400;
+const int MAX_HEIGHT = 400;
+
+// Bitmap class for everything about reading/writing .BIN
+class Bitmap
+{
+    private:
+        std::vector<std::vector<bool>> data;
+
+        int origWidth;
+        int origHeight;
+        int intWidth {MAX_WIDTH};
+        int intHeight {MAX_HEIGHT};
+
+    public:
+        Bitmap();
+
+        int getWidth() const { return intWidth; }
+        int getHeight() const { return intHeight; }
+        std::vector<std::vector<bool>> getData() const { return data; }
+
+        int getPixel(int x, int y);
+        bool read(const String &filename);
+        bool write(const String &filename);
+        void invertPixel(int x, int y);
+
+        std::vector<int> findEmptyRows();
+        std::vector<int> findEmptyCols();
+        void removeEmptyRowsAndColumns();
+};
+
+#endif // BITMAP_HPP
--- A_star/ard_astar/ard_astar.ino
+++ A_star/ard_astar/ard_astar.ino
@@ -1,9 +1,131 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+// Include necessary library headers
+// Note: You may need to implement or find Arduino-compatible versions of these
+#include <microTuple.h>
+#include "Bitmap.h"
+#include "Astar.h"
 
+struct step {
+    int angle;
+    int distance {1};
+    char direction;
+};
+
+// Function prototypes
+MicroTuple<char, int, String> calculateDirectionAngleAndHeading(int x1, int y1, int x2, int y2, const String& currentHeading);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, from Arduino maze solver!");
+
+  // Initialize your bitmap and A* objects here
+  Bitmap bmp;
+  Astar astar;
+
+  // You'll need to adapt the bitmap reading for Arduino
+  // This might involve reading from EEPROM or SD card
+  if (bmp.read("maze.bin")) {
+    Serial.println("Bitmap successfully read.");
+  }
+
+  bmp.removeEmptyRowsAndColumns();
+
+  Serial.print("Adjusted bitmap width: ");
+  Serial.print(bmp.getWidth());
+  Serial.print(" height: ");
+  Serial.println(bmp.getHeight());
+
+  auto astardata = bmp.getData();
+  astar.interpretBitmap(astardata);
+
+  Node* start = astar.determineStartNode();
+  Node* exit = astar.determineGoalNode();
+
+  Serial.print("startNode x:");
+  Serial.print(start->x);
+  Serial.print(" y:");
+  Serial.println(start->y);
+  Serial.print("exitNode x:");
+  Serial.print(exit->x);
+  Serial.print(" y:");
+  Serial.println(exit->y);
+
+  std::vector<Node*> path = astar.algorithm(start, exit);
+  std::vector<step> solution;
+
+  astar.printGrid(path);
+
+  char direction;
+  int angle;
+  String newHeading = "N";
+  String heading = newHeading;
+  
+  Serial.println("Path vector: (x,y)");
+  for (int i = 0 ; i < path.size()-1; ++i) {
+    Serial.print("(");
+    Serial.print(path[i]->x);
+    Serial.print(",");
+    Serial.print(path[i]->y);
+    Serial.print(") -> (");
+    Serial.print(path[i+1]->x);
+    Serial.print(",");
+    Serial.print(path[i+1]->y);
+    Serial.print(") ");
+    
+    MicroTuple<char, int, String> results = calculateDirectionAngleAndHeading(path[i]->x, path[i]->y, path[i+1]->x, path[i+1]->y, heading);
+    direction = results.get<0>();
+    angle = results.get<1>();
+    newHeading = results.get<2>();
+    heading = newHeading;
+    
+    Serial.print("Direction: ");
+    Serial.print(direction);
+    Serial.print(", Angle: ");
+    Serial.print(angle);
+    Serial.print(", New Heading: ");
+    Serial.println(newHeading);
+    
+    step val;
+    val.direction = direction;
+    val.angle = angle;
+    val.distance = 1;
+    solution.push_back(val);
+  }
+
+  // Simplify consecutive forward steps
+  for (int i = 0; i < solution.size()-1; ++i) {
+    if (solution[i].direction == 'F' && solution[i].direction == solution[i+1].direction) {
+      solution[i].distance++;
+      solution.erase(solution.begin() + i+1);
+      --i;
+    }
+  }
+
+  // Print the solution
+  for (int i = 0; i < solution.size(); ++i) {
+    Serial.print("step ");
+    Serial.print(i);
+    Serial.print(": angle: ");
+    Serial.print(solution[i].angle);
+    Serial.print(" direction: ");
+    Serial.print(solution[i].direction);
+    Serial.print(" distance: ");
+    Serial.println(solution[i].distance);
+  }
+
+  astar.cleanupGrid();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // The main loop can be left empty or used for repeated tasks
+}
 
+// Implement the calculateDirectionAngleAndHeading function here
+// You may need to adapt this function to use Arduino's String instead of std::string
+// and replace std::map with a simpler data structure if memory is a concern
+MicroTuple<char, int, String> calculateDirectionAngleAndHeading(int x1, int y1, int x2, int y2, const String& currentHeading) {
+  // Implementation goes here
+  // This function will need significant adaptation for Arduino
+  // You may need to use simpler data structures instead of std::map
+  // and implement your own string comparison logic
 }
