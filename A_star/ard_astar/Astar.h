c5136d0bb758d28bb09d4d10b47f1e77b67c1cb7
Matthew Ruiz <mrr171@txstate.edu>
2024-11-07 14:32:54 -0600

    Indent spacing and file.close where appropiate

--- A_star/ard_astar/Astar.h
+++ A_star/ard_astar/Astar.h
@@ -17,8 +17,16 @@ struct Node {
     : x(x), y(y), g(0.0), h(0.0), f(0.0), parent(nullptr), isObstacle(false), start(false), exit(false) {}
 };
 
+enum class HeuristicType {
+  MANHATTAN,
+  EUCLIDEAN,
+  CHEBYSHEV,
+  OCTILE
+};
+
 class Astar {
 private:
+  HeuristicType heuristicType = HeuristicType::OCTILE;
   std::vector<std::vector<Node*>> grid;
   int aWidth;
   int aHeight;
@@ -29,7 +37,10 @@ public:
   void initializeGrid();
   void cleanupGrid();
   bool isValid(int x, int y);
+  void setHeuristicType(HeuristicType type) {heuristicType = type;}
   float heuristic(int x1, int y1, int x2, int y2);
+  std::vector<Node*> smoothPath(const std::vector<Node*>& originalPath);
+  bool hasLineOfSight(Node* start, Node* end);
   std::vector<Node*> getNeighbors(Node* node);
   Node* findLowestF(std::vector<Node*>& openSet);
   std::vector<Node*> reconstructPath(Node* current);
--- A_star/ard_astar/Bitmap.cpp
+++ A_star/ard_astar/Bitmap.cpp
@@ -5,7 +5,7 @@ Bitmap::Bitmap() : origWidth(0), origHeight(0) {}
 
 int Bitmap::getPixel(int x, int y)
 {
-    return data[y][x] ? 1 : 0;
+  return data[y][x] ? 1 : 0;
 }
 
 // Read from SD card
@@ -35,6 +35,7 @@ bool Bitmap::read(File &file)
       data[y][x] = (c == '1');
     }
   }
+  file.close();
   return true;
 }
 
@@ -60,76 +61,77 @@ bool Bitmap::write(File &file)
       return false;
     }
   }
+  file.close();
   return true;
 }
 
 void Bitmap::invertPixel(int x, int y)
 {
-    if (x >= 0 && x < MAX_WIDTH && y >= 0 && y < MAX_HEIGHT)
-    {
-        data[y][x] = !data[y][x];
-    }
+  if (x >= 0 && x < MAX_WIDTH && y >= 0 && y < MAX_HEIGHT)
+  {
+    data[y][x] = !data[y][x];
+  }
 }
 
 std::vector<int> Bitmap::findEmptyRows()
 {
-    std::vector<int> emptyRows(MAX_HEIGHT, 0);
-    for (int y = 0; y < MAX_HEIGHT; ++y)
+  std::vector<int> emptyRows(MAX_HEIGHT, 0);
+  for (int y = 0; y < MAX_HEIGHT; ++y)
+  {
+    for (int x = 0; x < MAX_WIDTH; ++x)
     {
-        for (int x = 0; x < MAX_WIDTH; ++x)
-        {
-            if (data[y][x])
-            {
-                emptyRows[y] = 1;
-                break;
-            }
-        }
+      if (data[y][x])
+      {
+        emptyRows[y] = 1;
+        break;
+      }
     }
-    return emptyRows;
+  }
+  return emptyRows;
 } 
 
 std::vector<int> Bitmap::findEmptyCols()
 {
-    std::vector<int> emptyCols(MAX_WIDTH, 0);
-    for (int x = 0; x < MAX_WIDTH; ++x)
+  std::vector<int> emptyCols(MAX_WIDTH, 0);
+  for (int x = 0; x < MAX_WIDTH; ++x)
+  {
+    for (int y = 0; y < MAX_HEIGHT; ++y)
     {
-        for (int y = 0; y < MAX_HEIGHT; ++y)
-        {
-            if (data[y][x])
-            {
-                emptyCols[x] = 1;
-                break;
-            }
-        }
+      if (data[y][x])
+      {
+        emptyCols[x] = 1;
+        break;
+      }
     }
-    return emptyCols;
+  }
+  return emptyCols;
 }
 
 // Removes any row or column that is entirely 0
 void Bitmap::removeEmptyRowsAndColumns()
 {
-    std::vector<int> nonEmptyRows = findEmptyRows();
-    std::vector<int> nonEmptyCols = findEmptyCols();
+  std::vector<int> nonEmptyRows = findEmptyRows();
+  std::vector<int> nonEmptyCols = findEmptyCols();
 
-    // Remove empty rows
-    data.erase(
-        std::remove_if(data.begin(), data.end(),
-            [&nonEmptyRows, this](const std::vector<bool>& row) {
-                return nonEmptyRows[&row - &data[0]] == 0;
-            }),
-        data.end()
-    );
+  // Remove empty rows
+  data.erase(
+    std::remove_if(data.begin(), data.end(),
+      [&nonEmptyRows, this](const std::vector<bool>& row) {
+        return nonEmptyRows[&row - &data[0]] == 0;
+      }),
+    data.end()
+  );
 
-    // Remove empty columns
-    for (auto& row : data) {
-        for (int x = intWidth - 1; x >= 0; --x) {
-            if (nonEmptyCols[x] == 0) {
-                row.erase(row.begin() + x);
-            }
-        }
+  // Remove empty columns
+  for (auto& row : data) {
+    for (int x = intWidth - 1; x >= 0; --x) {
+      if (nonEmptyCols[x] == 0) {
+        row.erase(row.begin() + x);
+      }
     }
+  }
 
-    // Update dimensions
-    intHeight = data.size();
-    intWidth = data[0].size();
+  // Update dimensions
+  intHeight = data.size();
+  intWidth = data[0].size();
 }
